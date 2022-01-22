9761bc6aeb5aab3bfc45070394c779220b3bdf24
Petr Cezner <petr.cezner@factorio.cz>
2022-01-22 17:18:12 +0100

    add regex for numbers filtering

--- document_reader/main.py
+++ document_reader/main.py
@@ -1,17 +1,17 @@
 import cv2
 import pytesseract
+import re
 from document_straighter import straight
 
 
 def detect_text(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # blur = cv2.GaussianBlur(gray, (3, 3), 0)
-    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+    thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
-    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+    dilation = cv2.dilate(thresh, rect_kernel, iterations=1)
 
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
                                            cv2.CHAIN_APPROX_NONE)
@@ -25,19 +25,20 @@ def detect_text(image):
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
 
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
         cropped = im2[y:y + h, x:x + w]
         text = pytesseract.image_to_string(cropped)
-        with open("recognized.txt", "a") as f:
-            f.write(text)
-            f.write("\n")
+        text = re.findall("[+-]?\d+[.,:]?\d*(?:[eE][+-]?\d+)?", text)
+        if len(text) < 1:
+            continue
 
-        print(text)
+        with open("recognized.txt", "a") as f:
+            for t in text:
+                f.write(t)
+                f.write("\n")
 
 
 if __name__ == '__main__':
-    img = cv2.imread("data/sample_6.png")
+    img = cv2.imread("data/sample_5.jpg")
     cv2.imshow("Imported img", img)
     cv2.waitKey(0)
     img = straight(img)
