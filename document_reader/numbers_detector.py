ec2b6d98b0dfcf098897f06807470daee22b0805
Petr Cezner <petr.cezner@factorio.cz>
2022-01-22 18:30:55 +0100

    image upload and download after convertion

--- document_reader/numbers_detector.py
+++ document_reader/numbers_detector.py
@@ -1,10 +1,13 @@
+import datetime
+from pathlib import Path
+
 import cv2
 import pytesseract
 import re
-from document_straighter import straight
+from image_straighter import straight
 
 
-def detect_text(image):
+def detect_text(image, file_name: Path = Path('data/converted.txt'), language='eng'):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
@@ -17,8 +20,8 @@ def detect_text(image):
                                            cv2.CHAIN_APPROX_NONE)
 
     im2 = image.copy()
-
-    file = open("recognized.txt", "w+")
+    file_name.parent.mkdir(parents=True, exist_ok=True)
+    file = open(file_name, "w+")
     file.write("")
     file.close()
 
@@ -26,15 +29,22 @@ def detect_text(image):
         x, y, w, h = cv2.boundingRect(cnt)
 
         cropped = im2[y:y + h, x:x + w]
-        text = pytesseract.image_to_string(cropped)
+        text = pytesseract.image_to_string(cropped, lang=language)
         text = re.findall("[+-]?\d+[.,:]?\d*(?:[eE][+-]?\d+)?", text)
         if len(text) < 1:
             continue
 
-        with open("recognized.txt", "a") as f:
+        with open(file_name, "a") as f:
             for t in text:
                 f.write(t)
                 f.write("\n")
+    return file_name
+
+
+def convert_image(image, language='eng'):
+    image = straight(image)
+    result_path = detect_text(image, language=language)
+    return result_path
 
 
 if __name__ == '__main__':
@@ -42,4 +52,4 @@ if __name__ == '__main__':
     cv2.imshow("Imported img", img)
     cv2.waitKey(0)
     img = straight(img)
-    detect_text(img)
+    detect_text(img, Path('data'))
--- document_reader/streamlit_app.py
+++ document_reader/streamlit_app.py
@@ -1,10 +1,13 @@
+import cv2
+import numpy as np
 import streamlit as st
 import pandas as pd
 import datetime
 import argparse
 from pathlib import Path
-
+from numbers_detector import convert_image
 from utils import init_logger
+from PIL import Image
 
 logger = init_logger(__name__, testing_mode=False)
 # parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -16,7 +19,6 @@ logger = init_logger(__name__, testing_mode=False)
 st.set_page_config(
     page_title="Document Extractor",
     page_icon="🧊",
-    layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://github.com/petrcezner/document-scanner/issues',
@@ -27,25 +29,54 @@ st.set_page_config(
 
 st.title('Welcome to Document Extractor!!!')
 
-if 'upload_submit' not in st.session_state:
-    st.session_state.upload_submit = False
+if 'current_time' not in st.session_state:
+    st.session_state.current_time = datetime.datetime.now()
+
+if 'image_loaded' not in st.session_state:
+    st.session_state.image_loaded = False
+
+if 'converted' not in st.session_state:
+    st.session_state.converted = False
+
+if 'document_language' not in st.session_state:
+    st.session_state.document_language = 'eng'
 
 
-def on_data_upload_submit():
-    st.session_state.bucket = st.session_state.u_bucket
-    st.session_state.measurement = st.session_state.u_measurement
-    st.session_state.data_time = datetime.datetime.combine(st.session_state.u_date,
-                                                           st.session_state.u_time,
-                                                           tzinfo=datetime.datetime.now().astimezone().tzinfo)
-    st.session_state.uploaded_data = pd.read_csv(st.session_state.uploaded_file)
-    st.session_state.upload_submit = True
-    st.session_state.data_loaded = True
+def on_image_convert_submit():
+    st.session_state.current_time = datetime.datetime.now()
+    file_bytes = np.asarray(bytearray(st.session_state.image_path.read()), dtype=np.uint8)
+    st.session_state.loaded_image = cv2.imdecode(file_bytes, 1)
+    st.session_state.document_language = st.session_state.doc_language
+    st.session_state.image_loaded = True
 
 
-if st.sidebar.button('Upload data'):
+if st.button('Upload Image'):
     with st.form('upload_form'):
-        st.text_input('Warehouse', value='w0000', key='u_bucket')
-        st.text_input('Box Number', value='b0000', key='u_measurement')
-        st.file_uploader("Choose a file", key='uploaded_file', type=['jpg', 'png'])
-        # st.form_submit_button(label='Convert', on_click=on_data_upload_submit)
-        st.form_submit_button(label='Convert')
+        st.text_input('Warehouse', value='w0000', key='warehouse')
+        st.text_input('Box Number', value='b0000', key='box_number')
+        st.selectbox('Select document language',
+                     ['eng', 'deu', 'fra', 'ces', 'rus', 'pol'],
+                     key='doc_language')
+        st.file_uploader("Choose a file", key='image_path', type=['jpg', 'png'])
+        st.form_submit_button(label='Upload', on_click=on_image_convert_submit)
+
+if st.session_state.image_loaded:
+    st.image(st.session_state.loaded_image, channels="BGR")
+    if st.button('Convert'):
+        st.session_state.result_path = convert_image(st.session_state.loaded_image,
+                                                     language=st.session_state.document_language)
+        st.session_state.converted = True
+
+    if st.session_state.converted:
+        with open(st.session_state.result_path, 'r') as f:
+            uploaded_file = f.read()
+            st.markdown('## Found numbers in image:')
+            st.write(uploaded_file)
+
+        st.download_button(label='Download converted image',
+                           data=uploaded_file,
+                           file_name=f'converted_image_{st.session_state.current_time.strftime("%Y%m%d-%H%M%S")}.txt',
+                           mime='text/txt')
+if st.button('Close'):
+    st.session_state.image_loaded = False
+    st.session_state.converted = False
