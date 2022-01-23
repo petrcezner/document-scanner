511d75175c0cb55f328ad754fd37b6ac9c5f0b5a
Petr Cezner <petr.cezner@factorio.cz>
2022-01-23 11:42:31 +0100

    Feature/1 saving parsed text to csv (#2)
    * add streamlit frontend
    
    * add regex for numbers filtering
    
    * image upload and download after convertion
    
    * add docker file and logging of some informations
    
    * add saving of results into sqlite3 db

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10.2-buster
+
+LABEL version="2021.1" maintainer="petr.cezner@factorio.cz"
+
+COPY requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN apt-get update -y && apt-get install tesseract-ocr -y && apt-get install tesseract-ocr-all -y
+RUN pip install -r requirements.txt
+
+COPY document_reader/. /app/document_reader
+
+RUN python document_reader/document_db/init_db.py
+
+ENTRYPOINT [ "streamlit", "run" ]
+EXPOSE 8501
+CMD [ "document_reader/streamlit_app.py"]
---
+++ document_reader/numbers_detector.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import logging
+import cv2
+import pytesseract
+import re
+from image_straighter import straight
+
+logger = logging.getLogger('__main__.number_detector.py')
+
+
+def detect_text(image, file_name: Path = Path('data/converted.txt'), language='eng'):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+    thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+
+    dilation = cv2.dilate(thresh, rect_kernel, iterations=1)
+
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)
+
+    im2 = image.copy()
+    file_name.parent.mkdir(parents=True, exist_ok=True)
+    file = open(file_name, "w+")
+    file.write("")
+    file.close()
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        cropped = im2[y:y + h, x:x + w]
+        text = pytesseract.image_to_string(cropped, lang=language)
+        text = re.findall("[+-]?\d+[.,:]?\d*(?:[eE][+-]?\d+)?", text)
+        if len(text) < 1:
+            continue
+
+        with open(file_name, "a") as f:
+            for t in text:
+                f.write(t)
+                f.write("\n")
+    return file_name
+
+
+def convert_image(image, language='eng'):
+    image = straight(image)
+    logger.debug('Image is straighted.')
+    result_path = detect_text(image, language=language)
+    logger.info('Image is converted.')
+    return result_path
+
+
+if __name__ == '__main__':
+    img = cv2.imread("data/sample_5.jpg")
+    cv2.imshow("Imported img", img)
+    cv2.waitKey(0)
+    img = straight(img)
+    detect_text(img, Path('data'))
